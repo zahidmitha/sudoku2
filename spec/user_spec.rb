10821e91a7233247e2c4492516800f8d9be8ed1d
Leo Allen <leo.f.allen@gmail.com>
2013-03-27 14:41:39 +0000

    Adding in rspec

---
+++ spec/user_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require './lib/user'
+
+describe User do
+
+  let(:user) { User.new("Test Test") }
+
+  context "#new" do
+    it "sets name" do
+      user.name.should == "Test Test"
+    end
+  end
+end
